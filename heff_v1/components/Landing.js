fd0663afb7aedc7b1ba7b04023eef452234a4e23
techsavvyoutdoorsman <bramdonovan@gmail.com>
2022-03-20 01:06:04 -0600

    goody

--- heff_v1/components/Landing.js
+++ heff_v1/components/Landing.js
@@ -15,7 +15,7 @@ const bgSvg = '/landing_bg_5.svg'
 const LandingImg = () => {
     return (
         <Box zIndex={0} >
-            <Image layout='fill' src={bgSvg} alt='SVG background Image' priority/>
+            <Image objectFit='cover' layout='fill' src={bgSvg} alt='SVG background Image' priority/>
         </Box>
     )
 }
@@ -40,32 +40,29 @@ const Landing = () => {
         <Flex
         minH='100vh'
         minW='100vw'
-        // bgImg={bgSvg}
-        // bgRepeat='no-repeat'
-        // bgPosition='bottom'
-        // backgroundSize='cover'
         align='center'
         justify='center'
         position='relative'
         overflow='visible'
         >
         <LandingImg />
-        <Flex zIndex={3} w='100%' p='20px' maxW='container.lg' sx={{
-            flexDirection: 'column',
-            '@media(min-width: 768px)': {
-                flexDirection: 'row',
-                paddingLeft: '40px'
-            }
-        }}>
-            <Flex  align='center' justify='center' w='100%'>
-                <Flex flexDir='column' gap='20px'  textAlign='left' >
-                    <EyeCatch>Home Installations & Tech Support</EyeCatch>
-                    <Text as='p' color={useColorModeValue('black.900', 'white.100')}>Ames ac turpis egestas sed. Semper viverra nam libero justo laoreet sit amet cursus. Vitae tempus quam pellentesque nec nam. Lectus magna fringilla urna porttitor rhoncus dolor purus non.</Text>
-                    <Button h='100%' pt={.5} pb={.5} maxH='30px' maxW='90px' borderRadius='30px' bg='green.500' color={useColorModeValue('white.100', 'black.900')} _hover={{ bg: useColorModeValue('green.600', 'green.400'), color: useColorModeValue('white.200', 'black.800')}}>Contact</Button>
+            <Flex zIndex={3} w='100%' p='20px' maxW='container.lg' sx={{
+                flexDirection: 'column',
+                justifyContent: 'end',
+                '@media(min-width: 768px)': {
+                    flexDirection: 'row',
+                    paddingLeft: '40px'
+                }
+            }}>
+                <Flex align='center' justify='center' w='100%'>
+                    <Flex flexDir='column' gap='20px'  textAlign='left' >
+                        <EyeCatch>Home Installations & Tech Support</EyeCatch>
+                        <Text as='p' color={useColorModeValue('black.900', 'white.100')}>Ames ac turpis egestas sed. Semper viverra nam libero justo laoreet sit amet cursus. Vitae tempus quam pellentesque nec nam. Lectus magna fringilla urna porttitor rhoncus dolor purus non.</Text>
+                        <Button h='100%' pt={.5} pb={.5} maxH='30px' maxW='90px' borderRadius='30px' bg='green.500' color={useColorModeValue('white.100', 'black.900')} _hover={{ bg: useColorModeValue('green.600', 'green.400'), color: useColorModeValue('white.200', 'black.800')}}>Contact</Button>
+                    </Flex>
                 </Flex>
+                <Box w='50%'></Box>
             </Flex>
-            <Box w='500%'></Box>
-        </Flex>
         </Flex>
     )
 }
