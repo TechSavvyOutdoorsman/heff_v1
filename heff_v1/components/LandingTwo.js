6e7441009929b09eb9200e7d535236448eee0322
techsavvyoutdoorsman <bramdonovan@gmail.com>
2022-03-21 04:10:47 -0600

    how is this happening

--- heff_v1/components/LandingTwo.js
+++ heff_v1/components/LandingTwo.js
@@ -17,7 +17,7 @@ const blueBottom = '/blue-bottom.svg'
 const Top = () => {
     return (
         <Box zIndex={0} pos='relative' display='block' >
-            <Image objectFit='cover' layout='responsive' width={1439.7} height={311.5} src={blueTop} alt='Top of Blue Background' />
+            <Image objectFit='cover' layout='responsive' width={1439.9} height={311.5} src={blueTop} alt='Top of Blue Background' />
         </Box>
     )
 }
