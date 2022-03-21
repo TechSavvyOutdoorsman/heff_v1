88167f90a481d984f6276019ac6fc411e331c8f5
techsavvyoutdoorsman <bramdonovan@gmail.com>
2022-03-21 04:09:27 -0600

    coooom

--- heff_v1/components/LandingCard.js
+++ heff_v1/components/LandingCard.js
@@ -29,8 +29,8 @@ const LandingCard = () => {
                             padding: '5rem'
                         }
                     }}>
-                        <Heading lineHeight='2.25rem' fontSize={{ base: '33px', md: '40px', lg: '56px' }}>Home Installations & Tech Support</Heading>
-                        <Text lineHeight='1.25rem' fontSize={{ base: '18px', md: '24px',}} color={useColorModeValue('black.900', 'white.100')}>Ames ac turpis egestas sed. Semper viverra nam libero justo laoreet sit amet cursus. Vitae tempus quam pellentesque nec nam. Lectus magna fringilla urna porttitor rhoncus dolor purus non.</Text>
+                        <Heading lineHeight={{ base: '2.25rem', md: '3.3rem'}} fontSize={{ base: '33px', md: '40px', lg: '56px' }}>Home Installations & Tech Support</Heading>
+                        <Text lineHeight={{ base: '1.25rem', md: '1.65rem'}} fontSize={{ base: '18px', md: '24px',}} color={useColorModeValue('black.900', 'white.100')}>Ames ac turpis egestas sed. Semper viverra nam libero justo laoreet sit amet cursus. Vitae tempus quam pellentesque nec nam. Lectus magna fringilla urna porttitor rhoncus dolor purus non.</Text>
                         <Button h='100%' pt={2} pb={2} maxH='30px' maxW='90px' borderRadius='30px' bg='green.500' color={useColorModeValue('white.100', 'black.900')} _hover={{ bg: useColorModeValue('green.600', 'green.400'), color: useColorModeValue('white.200', 'black.800')}}>Contact</Button>
                     </Flex>
         </>
