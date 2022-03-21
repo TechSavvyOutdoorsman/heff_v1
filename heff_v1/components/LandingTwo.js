209189d981aad21214ff66f265e8180f4cd206b8
techsavvyoutdoorsman <bramdonovan@gmail.com>
2022-03-21 03:09:39 -0600

    good try

--- heff_v1/components/LandingTwo.js
+++ heff_v1/components/LandingTwo.js
@@ -17,7 +17,7 @@ const blueBottom = '/blue-bottom.svg'
 const Top = () => {
     return (
         <Box overflow='visible' m={0} p={0} zIndex={0} pos='relative' display='block' >
-            <Image objectFit='cover' layout='responsive' width={1440} height={360} src={blueTop} alt='Top of Blue Background' />
+            <Image objectFit='cover' layout='responsive' width={1440} height={311} src={blueTop} alt='Top of Blue Background' />
         </Box>
     )
 }
